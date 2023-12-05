27c6fa59106eef4c381d47d5f1a59dc3503fc043
AYIWAHOUN Codjo Espérance <125973194+Espe-dev@users.noreply.github.com>
2023-12-05 19:37:26 +0100

    Code test fonctionnel

--- moteurs.py
+++ moteurs.py
@@ -1,1 +1,65 @@
+import time
+import RPi.GPIO as GPIO
 
+Motor_A_EN    = 4
+Motor_B_EN    = 17
+
+Motor_A_Pin1  = 14
+Motor_A_Pin2  = 15
+
+Motor_B_Pin1  = 27
+Motor_B_Pin2  = 18
+
+pwn_A = 0
+pwm_B = 0
+
+def motorStop():#Motor stops
+	GPIO.output(Motor_A_Pin1, GPIO.LOW)
+	GPIO.output(Motor_A_Pin2, GPIO.LOW)
+	GPIO.output(Motor_B_Pin1, GPIO.LOW)
+	GPIO.output(Motor_B_Pin2, GPIO.LOW)
+	GPIO.output(Motor_A_EN, GPIO.LOW)
+	GPIO.output(Motor_B_EN, GPIO.LOW)
+
+
+def setup():#Motor initialization
+	global pwm_A, pwm_B
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(Motor_A_EN, GPIO.OUT)
+	GPIO.setup(Motor_B_EN, GPIO.OUT)
+	GPIO.setup(Motor_A_Pin1, GPIO.OUT)
+	GPIO.setup(Motor_A_Pin2, GPIO.OUT)
+	GPIO.setup(Motor_B_Pin1, GPIO.OUT)
+	GPIO.setup(Motor_B_Pin2, GPIO.OUT)
+
+	motorStop()
+	try:
+		pwm_A = GPIO.PWM(Motor_A_EN, 1000)
+		pwm_B = GPIO.PWM(Motor_B_EN, 1000)
+	except:
+		pass
+
+def tout_droit(vitesse):
+    GPIO.output(Motor_A_Pin1, GPIO.HIGH)
+    GPIO.output(Motor_A_Pin2, GPIO.LOW)
+    GPIO.output(Motor_B_Pin1, GPIO.HIGH)
+    GPIO.output(Motor_B_Pin2, GPIO.LOW)
+    pwm_A.ChangeDutyCycle(vitesse)
+    pwm_B.ChangeDutyCycle(vitesse)
+
+def destroy():
+	motorStop()
+	GPIO.cleanup()             # Release resource
+
+
+if __name__ == '__main__':
+	try:
+		setup()
+		tout_droit()
+      while True:
+             tout_droit(60)
+		motorStop()
+		destroy()
+	except KeyboardInterrupt:
+		destroy()
