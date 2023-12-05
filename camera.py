f0b714b99ce47c4feb95ad015242a631b2fbac10
AYIWAHOUN Codjo Espérance <125973194+Espe-dev@users.noreply.github.com>
2023-12-05 18:01:22 +0100

    Version finale camera.py

--- camera.py
+++ camera.py
@@ -1,68 +1,26 @@
 import time
-import threading
-import cv2
+import picamera
 
-class CameraEvent:
-    def __init__(self):
-        self.event = threading.Event()
-
-    def wait(self):
-        self.event.wait()
-
-    def set(self):
-        self.event.set()
-
-    def clear(self):
-        self.event.clear()
-
-class PiCamera:
-    thread = None
-    frame = None
-    event = CameraEvent()
-
-    @staticmethod
-    def _thread():
-        print('Démarrage du thread de la caméra.')
-        camera = cv2.VideoCapture(0)
-        while True:
-            _, frame = camera.read()
-            PiCamera.frame = frame
-            PiCamera.event.set()
-            time.sleep(1)  # capture une image toutes les une seconde
-
-    def __init__(self):
-        if PiCamera.thread is None:
-            PiCamera.thread = threading.Thread(target=self._thread, daemon=True)
-            PiCamera.thread.start()
-
-    def get_frame(self):
-        "retrourne une frame à une taille fixe (224x224)"
-        PiCamera.event.wait()
-        PiCamera.event.clear()
-
-        resized_frame = cv2.resize(PiCamera.frame, (224, 224))
-
-        return resized_frame
+# Initialisation de la caméra
+camera = picamera.PiCamera()
 
 try:
-    # Crée une instance de la caméra
-    camera = PiCamera()
-
-    time_debut = time.time()
-    # Effectue une boucle pour récupérer les images et afficher le résultat du test des servos
     while True:
-        frame = camera.get_frame()
-        cv2.imshow("Test Camera", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break  # Quitte la boucle si la touche 'q' est pressée
+        # Nom du fichier avec timestamp
+        timestamp = time.strftime("%Y%m%d%H%M%S")
+        filename = f"photo_{timestamp}.jpg"
 
-        # Quitte la boucle après 60 secondes
-        t = time.time() - time_debut
-        if t > 60:
-            break
+        # Capture et enregistrement de la photo
+        camera.capture(filename)
 
-except Exception as error:
-    cv2.destroyAllWindows()
-    raise error
+        # Attente de 5 secondes
+        time.sleep(5)
+
+except KeyboardInterrupt:
+    # Arrêt du programme en cas d'interruption (Ctrl+C)
+    pass
+
 finally:
-    cv2.destroyAllWindows()
+    # Fermeture de la caméra
+    camera.close()
+    
