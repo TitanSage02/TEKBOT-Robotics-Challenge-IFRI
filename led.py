271af447f5521098571bdf8e03795e39b2abc120
AYIWAHOUN Codjo Espérance <125973194+Espe-dev@users.noreply.github.com>
2023-12-05 20:14:47 +0100

    Update led.py

--- led.py
+++ led.py
@@ -1,55 +1,33 @@
-#!/usr/bin/python3
-# File name   : LED.py
-# Description : WS_2812
-# Website     : based on the code from https://github.com/rpi-ws281x/rpi-ws281x-python/blob/master/examples/strandtest.py
 import time
 from rpi_ws281x import *
-import argparse
 
-# LED strip configuration:
-LED_COUNT      = 12      # Number of LED pixels.
-LED_PIN        = 12      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_COUNT = 12      # Nombre de pixels LED.
+LED_PIN = 12        # Broche GPIO connectée aux pixels (18 utilise PWM!).
+LED_FREQ_HZ = 800000 # Fréquence du signal LED en hertz (généralement 800khz)
+LED_DMA = 10        # Canal DMA à utiliser pour générer le signal (essayez 10)
+LED_BRIGHTNESS = 255 # Réglez à 0 pour le plus sombre et à 255 pour le plus lumineux
+LED_INVERT = False   # True pour inverser le signal (lors de l'utilisation d'un décalage de niveau transistor NPN)
+LED_CHANNEL = 0      # Défini sur '1' pour GPIOs 13, 19, 41, 45 ou 53
 
 class LED:
     def __init__(self):
-        self.LED_COUNT      = 16      # Number of LED pixels.
-        self.LED_PIN        = 12      # GPIO pin connected to the pixels (18 uses PWM!).
-        self.LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-        self.LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-        self.LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-        self.LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-        self.LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
-        args = parser.parse_args()
-
-        # Create NeoPixel object with appropriate configuration.
-        self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT, self.LED_BRIGHTNESS, self.LED_CHANNEL)
-        # Intialize the library (must be called once before other functions).
+        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
         self.strip.begin()
 
-
-
     def colorWipe(self, R, G, B):
-        color = Color(R,G,B)
+        color = Color(R, G, B)
         for i in range(self.strip.numPixels()):
             self.strip.setPixelColor(i, color)
             self.strip.show()
 
 led = LED()
-  try:  
-      while True:  
-          led.colorWipe(255, 0, 0)  # red
-          time.sleep(1)  
-          led.colorWipe(0, 255, 0)  # green
-          time.sleep(1)  
-          led.colorWipe(0, 0, 255)  # blue
-          time.sleep(1) 
-  except:  
-      led.colorWipe(0,0,0)  # Lights out
+try:
+    while True:
+        led.colorWipe(255, 0, 0)  # Rouge
+        time.sleep(1)
+        led.colorWipe(0, 255, 0)  # Vert
+        time.sleep(1)
+        led.colorWipe(0, 0, 255)  # Bleu
+        time.sleep(1)
+except KeyboardInterrupt:
+    led.colorWipe(0, 0, 0)  # Extinction des lumières
