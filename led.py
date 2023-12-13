83f2017caad5132f49a7e70fd35f528c8fbb05e8
AYIWAHOUN Codjo Espérance <eayiwahoun@gmail.com>
2023-12-13 13:03:30 +0100

    LED

--- led.py
+++ led.py
@@ -1,10 +1,10 @@
 import time
 from rpi_ws281x import *
 
-LED_COUNT = 12      # Nombre de pixels LED.
-LED_PIN = 12        # Broche GPIO connectée aux pixels (18 utilise PWM!).
+LED_COUNT = 16       # Nombre de pixels LED.
+LED_PIN = 12         # Broche GPIO connectée aux pixels (18 utilise PWM!).
 LED_FREQ_HZ = 800000 # Fréquence du signal LED en hertz (généralement 800khz)
-LED_DMA = 10        # Canal DMA à utiliser pour générer le signal (essayez 10)
+LED_DMA = 10         # Canal DMA à utiliser pour générer le signal (essayez 10)
 LED_BRIGHTNESS = 255 # Réglez à 0 pour le plus sombre et à 255 pour le plus lumineux
 LED_INVERT = False   # True pour inverser le signal (lors de l'utilisation d'un décalage de niveau transistor NPN)
 LED_CHANNEL = 0      # Défini sur '1' pour GPIOs 13, 19, 41, 45 ou 53
